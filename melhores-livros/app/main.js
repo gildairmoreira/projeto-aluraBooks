eedae9d3282df6b26cd65414889580d21945e8ae
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-12-30 22:05:32 -0300

    mais uma parte do projeto

---
+++ melhores-livros/app/main.js
@@ -0,0 +1,12 @@
+let livros = []
+const endPointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaApi()
+
+
+async function getBuscarLivrosDaApi(){
+    const res = await fetch(endPointDaApi)
+    livros = await res.json()
+    let livosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livosComDesconto)
+}
+
